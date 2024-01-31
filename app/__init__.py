5d176471ed731da54ea00f088b62902f130d0f46
疯子XUFUYU <xufuyu-mg@outlook.com>
2024-01-31 13:37:05 +0800

    更新项目结构

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-# 创建app应用,__name__是python预定义变量，被设置为使用本模块.
-app = Flask(__name__)
-from app import routes
--- manage..py
+++ manage..py
@@ -1,18 +1,9 @@
-# 从app模块中导入app应用
-from app import app
-# 获取启动参数
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--host', required=False, default='0.0.0.0', help='Set IP')
-parser.add_argument('--port', required=False, default=8000, help='Set Port')
-# parser.add_argument('--cuda', action='store_true', help='enables cuda')
-opt = parser.parse_args()
-host=opt.host
-port=opt.port
-print("Running at", host, " ", port)
-# 启动
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host=host, port=port)
+    pass
 
 
 
